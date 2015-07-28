48a960209b23b81b61d1cf5efd80c5c4f6ad886f
Saworieza <saworieza@gmail.com>
2015-07-28 19:20:10 +0300

    working on the reply segment. has issue replying

--- app/controllers/convos_controller.rb
+++ app/controllers/convos_controller.rb
@@ -7,8 +7,14 @@ class ConvosController < ApplicationController
     	recipients = User.where(id: conversation_params[:recipients])
     	conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
     	flash[:success] = "Your message was successfully sent!"
-    	redirect_to convo_path(conversation)
+    	redirect_to convo_path(conversation) #conversation created in line 8
   	end
+
+    def reply
+      current_user.reply_to_conversation(conversation, message_params[:body])
+      flash[:notice] = "Your reply message was successfully sent!"
+      redirect_to convo_path(conversation)
+    end
 
   	def show
     	@receipts = conversation.receipts_for(current_user)
@@ -22,5 +28,9 @@ class ConvosController < ApplicationController
   	def conversation_params
     	params.require(:convo).permit(:subject, :body,recipients:[])
   	end
+
+    def message_params
+      params.require(:message).permit(:body, :subject)
+    end
 
 end
