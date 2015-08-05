8f37c0bf18a23a7ca5c47e99e1b4f778de677117
Saworieza <saworieza@gmail.com>
2015-08-05 12:36:05 +0300

    fixing the friendship issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!, except: [:index]
   def index
   	#@users = User.all.order('created_at desc')
   	#this  line hee is awesome as it prevens me from kujiona on my onw page. but, it is hell as it throws and error if user aint sined in
